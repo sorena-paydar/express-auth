256e11d173cb7e9570ea9f2430746964a32d562b
sorena <sorenapaydar81@gmail.com>
2022-05-05 20:22:33 +0430

    feat: add middlewares and load up controllers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,28 @@
 // express
-import express, { Application } from "express";
+import express, { Application, RequestHandler } from "express";
 
 // typings
 import Server from "./typings/Server";
 
 // utils
 import { PORT } from "./config";
+import Controller from "./typings/Controller";
+import AuthCotnroller from "./controllers/AuthController";
+import cors from "cors";
+import helmet from "helmet";
 
 const app: Application = express();
 const server: Server = new Server(app, PORT);
 
+const controllers: Array<Controller> = [new AuthCotnroller()];
+
+const globalMiddleware: Array<RequestHandler> = [
+  helmet(),
+  cors({ credentials: true, origin: true }),
+];
+
 Promise.resolve().then(() => {
+  server.loadMiddleware(globalMiddleware);
+  server.loadControllers(controllers);
   server.run();
 });
