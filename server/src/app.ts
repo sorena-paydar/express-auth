e53833bb34ec8f49c09f5866b5c1dfb0e1debb22
sorena <sorenapaydar81@gmail.com>
2022-05-19 20:19:47 +0430

    feat: add global middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,37 @@
 // express
 import express, { Application, RequestHandler } from "express";
 
+// global middlewares
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
 // typings
 import Server from "./typings/Server";
+import Controller from "./typings/Controller";
 
 // utils
 import { PORT } from "./config";
-import Controller from "./typings/Controller";
+import db from "./models";
+import passport from "passport";
+import "./config/passport";
+
+// controllers
 import AuthCotnroller from "./controllers/AuthController";
-import cors from "cors";
-import helmet from "helmet";
 
 const app: Application = express();
-const server: Server = new Server(app, PORT);
+const server: Server = new Server(app, db.sequelize, PORT);
 
 const controllers: Array<Controller> = [new AuthCotnroller()];
 
 const globalMiddleware: Array<RequestHandler> = [
   helmet(),
+  bodyParser.urlencoded({ extended: false }),
+  bodyParser.json(),
   cors({ credentials: true, origin: true }),
+  passport.initialize(),
+  morgan("dev"),
 ];
 
 Promise.resolve().then(() => {
