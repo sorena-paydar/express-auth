11b93662c98aec49246c2060c921c6ba9459c411
sorena <sorenapaydar81@gmail.com>
2022-06-09 19:43:12 +0430

    fix: read ssh private key

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,9 +3,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import fs from "fs";
+import path from "path";
 
 export const PORT: number = parseInt(process.env.PORT!);
 
 export const DATABASE_URL: string = process.env.DATABASE_URL!;
 
-export const PRIVATE_KEY: string = fs.readFileSync("rsa-private.key", "utf-8");
+export const PRIVATE_KEY: string = fs.readFileSync(
+  path.join(__dirname, "..", "..", "rsa-private.key"),
+  "utf-8"
+);
