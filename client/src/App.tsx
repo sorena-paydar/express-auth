b0bb911bc28d3caeebfdb842d742328d972bf853
sorena <sorenapaydar81@gmail.com>
2022-06-09 19:41:59 +0430

    feat: set up routes for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import Account from "./pages/account";
+import Home from "./pages/Home/home";
+import Login from "./pages/login";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/account" element={<Account />} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
